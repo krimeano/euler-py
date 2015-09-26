1c54ddb7ca1a2a9fa5dd3fb9937e697f85e1ff0b
krimeano <krimeano@gmail.com>
2015-09-26 22:11:37 +0100

    solved 71

---
+++ problem71.py
@@ -0,0 +1,27 @@
+__author__ = 'krimeano'
+
+import math, mylib
+
+
+def solve():
+    result = 0
+    f0 = 3 / 7
+    print(3, '/', 7, '=', f0)
+    print('-' * 79)
+    best_result = (1, 1, 1, 1, [], [])
+    for x in range(2, 1000000):
+        n = math.floor(f0 * x)
+        f = n / x
+        d = f0 - f
+        if d and d < best_result[2]:
+            best_result = (n, x, d, f, mylib.find_dividers(n, distinct=True), mylib.find_dividers(x, distinct=True))
+            print(best_result)
+    print('-' * 79)
+    print(best_result)
+    return best_result[0]
+
+
+if __name__ == '__main__':
+    r = solve()
+    print('\n' + '-' * 79)
+    print(r)
