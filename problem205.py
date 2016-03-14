34c8de58b9cbe8876b28ab5771728f8a613bceb7
krimeano <krimeano@gmail.com>
2016-03-14 23:52:02 +0000

    solved 205

--- problem205.py
+++ problem205.py
@@ -7,36 +7,19 @@
 """
 
 
-def calc_outs(dice, outs=(1,)):
-    affix = (0,) * (dice - 1)
-    old_outs = (0,) + affix + outs + affix
-    new_outs = tuple()
-    for i in range(len(old_outs) - dice + 1):
-        out = 0
-        for j in range(dice):
-            out += old_outs[i + j]
-        new_outs = new_outs + (out,)
-    # print(dice, old_outs, new_outs)
+def calc_outs_next_dice(dice, outs=(1,)):
+    affix = (0,) * dice
+    outs = affix + outs + affix
+    new_outs = tuple(sum(outs[i + j] for j in range(dice)) for i in range(len(outs) - dice + 1))
     return new_outs
 
 
 def get_outs(dice, dices):
     outs = (1,)
     for i in range(dices):
-        outs = calc_outs(dice, outs)
+        outs = calc_outs_next_dice(dice, outs)
     return outs
 
 
-outs49 = get_outs(4, 9)
-sum49 = sum(outs49)
-print(sum49, outs49)
-outs66 = get_outs(6, 6)
-sum66 = sum(outs66)
-print(sum66, outs66)
-"""
-calc wins for outs
-"""
-s = 0
-for x in range(len(outs49)):
-    s += outs49[x] * sum(outs66[:x])
-print(s / sum66 / sum49)
+outs4x9, outs6x6 = get_outs(4, 9), get_outs(6, 6)
+print('%.7f' % (sum(outs4x9[x] * sum(outs6x6[:x]) for x in range(len(outs4x9))) / sum(outs6x6) / sum(outs4x9)))
