4684fe9dc3b67b82d3b2a4f83ce2531418f01443
palutser <spalutin@ft-services.com>
2016-04-06 18:48:35 +0100

    74

--- problem74.py
+++ problem74.py
@@ -34,24 +34,34 @@ def next_n(n):
 
 
 loop_nn = dict()
-
+lengths = dict()
 # xx = [145, 169, 871, 872, 69, 78, 540]
-xx = range(1000000)
-rr = []
+xx = range(1, 1000000)
+rr = 0
 for x in xx:
     ch = []
     next_x = 0
-    while next_x not in ch and next_x not in loop_nn:
+    while next_x not in ch and next_x not in loop_nn and next_x not in lengths:
         ch.append(next_x or x)
         next_x = next_n(next_x or x)
+
     i = len(ch)
-    if next_x not in loop_nn:
-        i = ch.index(next_x)
-        for y in ch[i:]:
-            loop_nn[y] = len(ch) - i
-    s = i + loop_nn[next_x]
+    if next_x in lengths:
+        s = len(ch) + lengths[next_x]
+    else:
+
+        if next_x not in loop_nn:
+            i = ch.index(next_x)
+            for y in ch[i:]:
+                loop_nn[y] = len(ch) - i
+                lengths[y] = len(ch) - i
+        s = i + loop_nn[next_x]
+    lengths[x] = s
+    for j in range(i):
+        lengths[ch[j]] = s - j
     if s == 60:
-        print(x, s, ch, next_x)
-        rr.append((x, ch, next_x,))
+        rr += 1
+        print(x, s, next_x, ch)
 print(loop_nn)
-print(len(rr))
+# print(lengths)
+print(rr)
