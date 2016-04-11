6be7cf92ba007d93af97680bc1e9cf20f787076d
palutser <spalutin@ft-services.com>
2016-04-11 16:57:27 +0100

    86

--- problem86.py
+++ problem86.py
@@ -27,15 +27,15 @@ m_max = M
 """
 import math, mylib
 
-M = 100
-ss = dict((x ** 2, True) for x in range(1, 2*M + 1))
+M = 2000
+ss = dict((x ** 2, True) for x in range(1, 3 * M + 1))
 print(M, ss)
 tt = []
 
 
 def find_mm_nn(a):
     out = []
-    n_max = math.floor(min(((a - 1) / 2), a ** 0.5))
+    n_max = math.floor(max(((a - 1) / 2), a ** 0.5))
     if not n_max:
         return out
     for n in range(1, n_max + 1):
@@ -70,9 +70,51 @@ def find_2mn(a):
     return out
 
 
+def find_brute(a):
+    out = []
+    for b in range(1, a + 1):
+        for c in range(1, b + 1):
+            s2 = a ** 2 + (b + c) ** 2
+            if s2 not in ss:
+                continue
+            t = (a, b, c)
+            # print(t, s2)
+            out.append(t)
+    return out
+
+
+ttb = []
+for a in range(1, 100 + 1):
+    ttb += find_brute(a)
+print(len(ttb))
+
+uu = []
+for t in ttb:
+    u = (t[0], t[1] + t[2])
+    if u not in uu:
+        uu.append(u)
+
 y = 0
+uu1 = []
+stored_tt = dict()
 for a in range(1, M + 1):
-    tt = find_mm_nn(a) + find_2mn(a)
+    clean_tt = find_mm_nn(a) + find_2mn(a)
+
+    stored_tt[a] = clean_tt
+    tt = clean_tt[:]
+    dd = mylib.find_composite_dividers(a)
+    for d in dd:
+        if d < 3 or d == a:
+            continue
+        if d in stored_tt:
+            # print(a, 'stored', stored_tt[d])
+            for t in stored_tt[d]:
+                t1 = (a, t[1] * a // d)
+                if t1 not in tt:
+                    # print(a, ' <- ', t1)
+                    tt.append(t1)
+                # tt += [x for x in stored_tt[d]]
+    uu1 += tt
     if not len(tt):
         continue
     z = 0
@@ -81,7 +123,12 @@ for a in range(1, M + 1):
         k = p[1] // 2
         if p[1] > p[0]:
             k -= p[1] - p[0] - 1
-        print(' ', p, k)
+        # k *= 3
+        #
+        # print(' ', p, k)
         z += k
     y += z
     print(a, tt, z, y)
+
+# print(uu)
+# print(uu1)
