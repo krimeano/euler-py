89ad2d6df9b6ccab213fb414dc49c6926fe0671b
palutser <spalutin@ft-services.com>
2016-04-26 10:44:36 +0100

    125

--- problem125.py
+++ problem125.py
@@ -3,23 +3,20 @@ import math
 m = 10 ** 8
 n_max = math.floor(((2 * m - 1) ** 0.5 + 1) / 2)
 print(m, n_max, n_max ** 2, n_max ** 2 + (n_max - 1) ** 2, n_max ** 2 + (n_max + 1) ** 2)
-s = 0
-ss = [0]
-for i in range(1, n_max + 2):
-    s += i ** 2
-    ss.append(s)
-    print(i, i ** 2, s)
-k = 0
+ss = [i ** 2 for i in range(n_max + 1)]
+
+print(ss[:5], '.' * round(math.log((len(ss) - 10), 10)), ss[-5:])
+
 r = 0
-for i in range(2, len(ss)):
-    for j in range(i - 2, -1, -1):
-        a = ss[i] - ss[j]
-        if a > m:
+for i in range(n_max, 1, -1):
+    s = ss[i]
+    for j in range(i - 1, 0, -1):
+        s += ss[j]
+        if s > m:
             break
-        if str(a) == str(a)[::-1]:
-            r += a
-            k += 1
-            # print(a, end=" ")
-            print(i, '-', j, ':', ss[i], '-', ss[j], '=', a, k, r)
-            # 2916867073
-print("\n\n", r)
+        if str(s) == str(s)[::-1]:
+            r += s
+            print(i, j, s, r)
+
+print(r)
+# 2916867073
