d969d3310ebd253e0bc24ad5044c173b4e1308df
krimeano <krimeano@gmail.com>
2019-05-26 12:41:40 +0100

    387

--- problem387.py
+++ problem387.py
@@ -25,84 +25,80 @@ import math, mylib
 class Harshad:
     def __init__(self, number):
         self.n = number
-        self.is_harshad = not self.n % len(str(self.n))
+        self.sum = self.to_sum()
+        self.is_harshad = not self.n % self.sum
 
     def __str__(self):
         return str(self.n)
 
     def is_right(self):
-        return self.n < 10 or (self.is_harshad and Harshad(self.n // 10).is_harshad)
+        return self.n > 0 and (self.n < 10 or (self.is_harshad and Harshad(self.n // 10).is_harshad))
 
     def is_strong(self):
-        return self.is_harshad and mylib.is_prime(self.n // len(str(self.n)))
+        return self.is_harshad and mylib.is_prime(self.n // self.sum)
+
+    def to_sum(self):
+        return sum([int(z) for z in str(self.n)])
+
+    def gen_next_right_harshads(self):
+        n = self.n * 10
+        return dict((y, True) for y in [Harshad(x) for x in range(n, n + 10)] if y.is_harshad)
+
+    def gen_next_primes(self):
+        return dict((y, True) for y in [self.n * 10 + x for x in (1, 3, 7, 9)] if mylib.is_prime(y))
 
 
-class Problem387:
-    hhh = [[], [1, 2, 3, 4, 5, 6, 7, 8, 9]]
-    ss = []
-    pp = dict()
-    rr = []
+class Problem387():
+    hhh = [dict(), dict((Harshad(x), True) for x in range(1, 10))]
+    ss = dict()
+    rr = dict()
+
+    @staticmethod
+    def init_primes(p):
+        mylib.known_primes = mylib.primes_from_file(10 ** math.ceil(p / 2))
+        print('primes:', '... ' + ', '.join([str(x) for x in (sorted(mylib.known_primes)[-10:])]))
 
     def make_right_harshads(self, p):
+        print('make right harshads', p)
         if len(self.hhh) < p:
             self.make_right_harshads(p - 1)
-        gg = self.hhh[p - 1]
-        hh = []
-        for g in gg:
-            hh += self.gen_next_right_harshads(p, g)
+        hh = dict()
+        for g in self.hhh[p - 1]:
+            hh.update(g.gen_next_right_harshads())
         self.hhh.append(hh)
         return self
 
-    @staticmethod
-    def gen_next_right_harshads(p, g):
-        out = []
-        n = 10 * g
-        r = n % p
-        if not r:
-            out.append(n)
-        out += [n + x for x in range(p - r, 10, p)]
-        print(p, g, out)
-        return out
-
     def filter_strong(self):
+        print('filter strong')
+        self.ss = dict()
         for i in range(1, len(self.hhh)):
-            ss = [x for x in self.hhh[i] if (x // i) in self.pp]
-            print('primes', i, ss)
-            self.ss += ss
-            print('NOT primes', i, [x for x in self.hhh[i] if (x // i) not in self.pp])
+            print('\t', i, len(self.hhh[i]))
+            self.ss.update(dict((x, True) for x in self.hhh[i] if x.is_strong()))
         return self
 
     def make_primes(self):
-        self.rr = []
-        for x in self.ss:
-            print(x, ':')
-            for y in (1, 3, 7, 9):
-                r = 10 * x + y
-                if r in self.pp:
-                    # if mylib.is_prime(r):
-                    print('+', r, 'is prime')
-                    self.rr.append(r)
-                else:
-                    print('-', r, 'is NOT prime', mylib.find_dividers(r))
+        print('make primes')
+        self.rr = dict()
+        for s in self.ss:
+            print('make primes for', s.n)
+            self.rr.update(s.gen_next_primes())
         return self
 
     def test(self):
         p = 4
-        self.pp = mylib.make_primes_sieve_atkin(10 ** p)
+        self.init_primes(p)
         self.make_right_harshads(p - 1).filter_strong().make_primes()
         for hh in self.hhh:
-            print(hh)
-        print(self.ss)
-        print(sum(self.rr), 90619 - sum(self.rr), self.rr)
-        # h = Harshad(3608528850368400786036725)
-        # print(h, h.is_harshad, h.is_right())
-        # print(len(pp), sum(len(hh) for hh in self.hhh))
-        # print(pp)
-        # print(self.hhh)
-        return False
+            print([h.n for h in hh])
+        print(sorted(s.n for s in self.ss))
+        print(sum(self.rr), 90619 - sum(self.rr), sorted(self.rr))
+        return 90619 == sum(self.rr)
 
     def solve(self):
-        return 0
+        p = 14
+        self.init_primes(p)
+        self.make_right_harshads(p - 1).filter_strong().make_primes()
+        return sum(self.rr)
 
 
 if __name__ == '__main__':
