5ab9f0b737f1ceb87425101344ce20517f4b1092
krimeano <krimeano@gmail.com>
2019-05-28 22:38:45 +0100

    solving

--- problem500.py
+++ problem500.py
@@ -1,39 +1,28 @@
-import mylib
+import mylib, math
 
-deuces = [2 ** i for i in range(100)]
-found = dict()
-found_deuces = dict()
+m = 500500507
+t = 500500
+pp = sorted(mylib.primes_from_file(10 ** 7))[:t]
+p_max = max(pp)
+p_max_r = math.ceil(p_max ** 0.5)
+print(len(pp), p_max, p_max_r, p_max_r ** 2)
+rr = pp[:t]
+qq = [q for q in pp if q <= p_max_r]
+print(qq)
 
-# step = 1 * 2 * 3 * 5 * 7 * 11 * 13 * 17 * 19
-# print(step)
-# for i in range(step, 10 ** 11, step):
-#     if not i % (step * 1000):
-#         print(i)
-#     dd = mylib.find_composite_dividers(i)
-#     s = len(dd)
-#     if s not in found:
-#         found[s] = True
-#         if s in deuces:
-#             found_deuces[s] = i
-#
-# for s in sorted(found_deuces):
-#     n = found_deuces[s]
-#     print(s, n, mylib.gather_dividers(n), mylib.find_composite_dividers(n))
-# # data = dict()
-# #
-# # for i in range(1, 2000000):
-# #     dd = mylib.find_composite_dividers(i)
-# #     s = len(dd)
-# #     if s not in data:
-# #         print(s, i, dd)
-# #         data[s] = []
-# #     if len(data[s]) < 5:
-# #         data[s].append(i)
-# #
-# # for s in sorted(data):
-# #     print(s, '>', data[s])
-#
-# numbers = (1, 2, 6, 24, 120, 840, 7560, 83160, 1081080)
-#
-# for n in numbers:
-#     print(n, mylib.gather_dividers(n))
+for q in qq:
+    x = q ** 2
+    while x < p_max:
+        print(q, x)
+        rr.append(x)
+        x = x ** 2
+
+rr = sorted(rr)[:t]
+print(max(rr))
+s = 1
+for i in range(len(rr)):
+    r = rr[i]
+    if not i % 10:
+        print(i, r)
+    s = (s * r) % m
+print(s)
