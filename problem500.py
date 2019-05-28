93c1b0c76400df682eba8ef95b228a98757ec842
krimeano <krimeano@gmail.com>
2019-05-28 21:57:08 +0100

    solving

--- problem500.py
+++ problem500.py
@@ -3,19 +3,22 @@ import mylib
 deuces = [2 ** i for i in range(100)]
 found = dict()
 found_deuces = dict()
-for i in range(210, 100000000, 210):
-    if not i % 210000:
-        print(i)
-    dd = mylib.find_composite_dividers(i)
-    s = len(dd)
-    if s not in found:
-        found[s] = True
-        if s in deuces:
-            found_deuces[s] = i
 
-for s in sorted(found_deuces):
-    n = found_deuces[s]
-    print(s, n, mylib.gather_dividers(n), mylib.find_composite_dividers(n))
+# step = 1 * 2 * 3 * 5 * 7 * 11 * 13 * 17 * 19
+# print(step)
+# for i in range(step, 10 ** 11, step):
+#     if not i % (step * 1000):
+#         print(i)
+#     dd = mylib.find_composite_dividers(i)
+#     s = len(dd)
+#     if s not in found:
+#         found[s] = True
+#         if s in deuces:
+#             found_deuces[s] = i
+#
+# for s in sorted(found_deuces):
+#     n = found_deuces[s]
+#     print(s, n, mylib.gather_dividers(n), mylib.find_composite_dividers(n))
 # # data = dict()
 # #
 # # for i in range(1, 2000000):
